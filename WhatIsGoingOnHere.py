2a1cfc4944fc0cb4c3321185c7f54ad8e5aee2be
AlexKent89 <137422029+AlexKent89@users.noreply.github.com>
2023-07-24 17:48:13 +0100

    Add files via upload

---
+++ WhatIsGoingOnHere.py
@@ -0,0 +1,7 @@
+
+#This will tell the program to print how many characters in the word hello which has been defined as Tup
+tup = 'Hello'
+print(len(tup))
+#This has a commer after which defines tup as a collection and in the collection there's only one word to count so 1 is outputted.
+tup = 'Hello',
+print(len(tup))
---
+++ WhatIsWrongWithThis.py
@@ -0,0 +1,12 @@
+
+# Oke should be on the end of the collection
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+print(cheese)
+# This is the correct way of adding 'Oke', with []
+cheese  += ['Oke']
+#cheese.append('Oke')
+#cheese.extend('Oke')
+print(cheese)
+# This is to add more than one cheese in one set of commands
+cheese  += ['Gouda','Edam']
+print(cheese)
